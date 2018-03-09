270fc8d55d903a16909ab80bf8d907aa01f11cca
jaroslaw.borelowski <jborelo@gmail.com>
2018-03-09 11:57:37 +0100

    " was missing"

--- simplequeue.py
+++ simplequeue.py
@@ -17,8 +17,8 @@ class SimpleQue(object):
     def __init__(self):
         self.dirMessages = ""
         self.filesExt = ""
-        self.err_message = "
-        self.bad_files = []    # contains names of problematic files which could not be read previously
+        self.err_message = ""
+        self.bad_files = []   # contains names of problematic files which could not be read previously
         self.loggerName = ""
         self.configured = False
 
