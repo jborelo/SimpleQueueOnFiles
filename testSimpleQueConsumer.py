47de3c4fe669213da89c2afb9c8d0d688c2f578a
jaroslaw.borelowski <jborelo@gmail.com>
2018-03-14 16:02:27 +0100

    time measuring added

--- testSimpleQueConsumer.py
+++ testSimpleQueConsumer.py
@@ -1,20 +1,53 @@
 from simplequeue import SimpleQue
 import time
+import datetime
+import json
 
 # logging.basicConfig(level=logging.DEBUG)
 
-sq = SimpleQue()
-sq.configure("que", create_dir=False)
+# -------------------------------------------------------
+def unpack(jsonstr_message):
+    """
+
+    :param jsonstr_message:
+    :return: tuple (content, timediff in microseconds)
+    """
+    # extract time and content
+    dtpop = datetime.datetime.now()  # time of pop
+    di = json.loads(message)
+    dtpush = strtodatetime(di[SimpleQue.KEY_TIME])  # time of push
+    diff = (dtpush - dtpop).microseconds
+    text = di[SimpleQue.KEY_CONTENT]
+    return (text, diff)
 
 
-if not sq.configured:
-    print(f"Problem with configuration: {sq.err_message}")
-    exit()
+# -----------------------------------------------------
+def pop_messages(sq, verbose=True, waitTime=1):
+    while True:
+        #get message
+        message = sq.pop()
 
-while True:
-    message = sq.pop()
-    if message:
-        print(message)
-    else:
-        print("No message")
-        time.sleep(5)
+        if message:
+            text, diff = unpack(message)
+            print(f"{text} - {diff}")
+        else:
+            print("No message")
+            time.sleep(waitTime)
+
+
+# ----------------------------------------------------------------
+def strtodatetime(text):
+    return datetime.datetime.strptime(text, "%Y-%m-%d %H:%M:%S.%f")
+
+
+# ------------------------------------------------------------
+if __name__ == "__main__":
+    sq = SimpleQue()
+    sq.configure("que", create_dir=False)
+
+    if not sq.configured:
+        print(f"Problem with configuration: {sq.err_message}")
+        exit()
+
+    pop_messages(sq)
+
--- testSimpleQueProducer.py
+++ testSimpleQueProducer.py
@@ -1,23 +1,68 @@
 from simplequeue import SimpleQue
 import time
+import datetime
+import json
 
-# create Que object
-sq = SimpleQue()
 
-sq.configure("que", create_dir=True)
+# --------------------------------------------------------
+def push_messages(queue, verbose=True, nb=100000, waitTime=0):
+    """
+    :waitTime in seconds, time between pushing
+    :param queue:
+    :param verbose:
+    :param nb:  max number of messages to  be inserted into queue
+    :return: True if all messages were sucesfully inserted into queue, or  False is et least one failed
+    """
+    """
 
-if not sq.configured:
-    print("Problem with configuration")
-    print(sq.err_message)
-    exit()
+    :param queue:
+    
+    """
 
-# push several messages
-cntr = 0
-while True:
-    message = f"Message: {cntr}"
-    cntr += 1
-    print(f"Pushing message: {message}")
-    if not sq.push(message):
-        print(f"ERROR !   ..{sq.err_message}..")
+    cntr = 0
+    while cntr < nb:
+        cntr += 1
 
-    time.sleep(1)
+        message = generate_message(cntr)
+        if verbose:
+            print(f"Pushing message: {message}")
+
+        b = sq.push(message)
+
+        if not b:
+            print(f"Pushing message error!! : ..{sq.err_message}..")
+        time.sleep(waitTime)
+
+
+# --------------------------------------------------------------------
+def generate_message(cnter):
+    di = {}
+    di[SimpleQue.KEY_CONTENT] = f"Message: {cnter}"
+    dt = datetime.datetime.now()
+    di[SimpleQue.KEY_TIME] = dt.__str__()
+    message = json.dumps(di, indent=4, default=myconverter)
+    return message
+
+
+# -------------------------------------------------------------
+def myconverter(o):
+    """        converts datatime object to string
+    :param o:
+    :return: str
+    """
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+
+if __name__ == "__main__":
+
+    sq = SimpleQue()
+    sq.configure("que", create_dir=True)
+
+    if not sq.configured:
+        print("Problem with configuration")
+        print(sq.err_message)
+        exit()
+
+    print("Starting test")
+    push_messages(sq, waitTime=1)
