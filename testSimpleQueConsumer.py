6b1072435e2fc21194442e5321f006a1abe6e39a
jaroslaw.borelowski <jborelo@gmail.com>
2018-03-14 17:18:02 +0100

    tests moved to classes

--- testSimpleQueConsumer.py
+++ testSimpleQueConsumer.py
@@ -3,52 +3,54 @@ import time
 import datetime
 import json
 
-# logging.basicConfig(level=logging.DEBUG)
 
-# -------------------------------------------------------
-def unpack(jsonstr_message):
-    """
+class Consumer:
 
-    :param jsonstr_message:
-    :return: tuple (content, seconds, microseconds)
-    """
-    # extract time and content
-    dtpop = datetime.datetime.now()  # time of pop
-    di = json.loads(jsonstr_message)
-    dtpush = strtodatetime(di[SimpleQue.KEY_TIME])  # time of push
-    diff = dtpop - dtpush
-    text = di[SimpleQue.KEY_CONTENT]
-    return (text, diff.seconds, diff.microseconds)
+    def setInputQueue(self, queu):
+        self.__sq__ = queu
 
+    @staticmethod
+    def unpack(jsonstr_message):
+        """
 
-# -----------------------------------------------------
-def pop_messages(sq, verbose=True, waitTime=1):
-    while True:
-        #get message
-        message = sq.pop()
+        :param jsonstr_message:
+        :return: tuple (content, seconds, microseconds)
+        """
+        # extract time and content
+        dtpop = datetime.datetime.now()  # time of pop
+        di = json.loads(jsonstr_message)
+        dtpush = Consumer.strtodatetime(di[SimpleQue.KEY_TIME])  # time of push
+        diff = dtpop - dtpush
+        text = di[SimpleQue.KEY_CONTENT]
+        return (text, diff.seconds, diff.microseconds)
 
-        if message:
-            text, sec, microsec = unpack(message)
-            print(f"{text} - {sec}, {microsec}")
-        else:
-            pass
-            #print("No message")
-            #time.sleep(waitTime)
+    def pop_messages(self, verbose=True, waitTime=1):
+        while True:
+            # get message
+            message = self.__sq__.pop()
 
+            if message:
+                text, sec, microsec = Consumer.unpack(message)
+                print(f"{text} - {sec}, {microsec}")
+            else:
+                pass
+                # print("No message")
+                # time.sleep(waitTime)
 
-# ----------------------------------------------------------------
-def strtodatetime(text):
-    return datetime.datetime.strptime(text, "%Y-%m-%d %H:%M:%S.%f")
+    @staticmethod
+    def strtodatetime(text):
+        return datetime.datetime.strptime(text, "%Y-%m-%d %H:%M:%S.%f")
 
 
 # ------------------------------------------------------------
 if __name__ == "__main__":
+
     sq = SimpleQue()
     sq.configure("que", create_dir=False)
-
     if not sq.configured:
         print(f"Problem with configuration: {sq.err_message}")
         exit()
 
-    pop_messages(sq)
-
+    consumer = Consumer()
+    consumer.setInputQueue(sq)
+    consumer.pop_messages()
